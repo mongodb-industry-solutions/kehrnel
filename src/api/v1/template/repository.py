4e450d41cb05e669f2169cadea9b9c897d320b0d
Giovanni Rodriguez <gio.rodriguez@mongodb.com>
2025-07-31 14:28:43 -0600

    Added the find_template_by_id and insert_template functions to the repository.py

---
+++ src/api/v1/template/repository.py
@@ -0,0 +1,39 @@
+from motor.motor_asyncio import AsyncIOMotorDatabase
+from pymongo.errors import PyMongoError
+import logging
+
+# Create a logger instace
+logger = logging.getLogger(__name__)
+
+TEMPLATE_COLL_NAME = "templates"
+
+async def find_template_by_id(template_id: str, db: AsyncIOMotorDatabase):
+    """
+    Retrieves a single template document fron the database by its template_id.
+    """
+    try:
+        template = await db[TEMPLATE_COLL_NAME].find_one({"_id": template_id})
+        if template:
+            return template
+        else:
+            logger.warning(f"Template with id {template_id} not found.")
+            return None
+    except PyMongoError as e:
+        logger.error(f"Error retrieving template with id {template_id}: {e}")
+        raise e
+    
+
+async def insert_template(template_doc: dict, db: AsyncIOMotorDatabase):
+    """
+    Inserts a new template document into the database.
+    Since this is a single operation, a transaction is not strictly necessary
+    but could be added if more operations were grouped with it.
+    """
+    
+    try:        
+        result = await db[TEMPLATE_COLL_NAME].insert_one(template_doc)
+        logger.info(f"Template with id: {result.inserted_id} inserted successfully.")
+    except PyMongoError as e:
+        logger.error(f"Template insertion failed: {e}")
+        # Re-raise the exception for the service layer to handle
+        raise
