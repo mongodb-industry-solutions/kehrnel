8c520cd1b33753d04520cc329b47ce67d5f7a95d
Giovanni Rodriguez <gio.rodriguez@mongodb.com>
2025-07-31 18:11:07 -0600

    Added the template_router to the FastAPI app

--- src/app/main.py
+++ src/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.app.core.database import connect_to_mongo, close_mongo_connection
 from src.api.v1.ehr.routes import router as ehr_router
+from src.api.v1.template.routes import router as template_router
 
 app = FastAPI(
     title="OpenEHR MongoDB API",
@@ -26,6 +27,7 @@ async def shutdown_event():
     await close_mongo_connection()
 
 app.include_router(ehr_router, prefix="/v1")
+app.include_router(template_router, prefix="/v1")
 
 # Test with Curl
 # curl -v -X POST http://localhost:8000/v1/ehr -H "Content-Type: application/json"
