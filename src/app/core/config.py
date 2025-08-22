df1064b7a29ab213e612dc0739d14223b806bf8c
Giovanni Rodriguez <gio.rodriguez@mongodb.com>
2025-08-22 15:16:57 -0600

    Added the extra = "ignore" flag to the Config class

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -2,11 +2,12 @@ from pydantic import Field
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    MONGODB_URI: str = Field(..., env="MONGODB_URI")
-    MONGODB_DB: str = Field(..., env="MONGODB_DB")
+    MONGODB_URI: str = Field(..., alias="MONGODB_URI")
+    MONGODB_DB: str = Field(..., alias="MONGODB_DB")
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
+        extra = "ignore"
 
 settings = Settings()
