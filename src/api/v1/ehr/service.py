415bf930421e059eb50d7ade68dcf399dbced424
Giovanni Rodriguez <gio.rodriguez@mongodb.com>
2025-09-09 16:39:47 -0600

    Added the retrieve_contribution to the service layer

--- src/api/v1/ehr/service.py
+++ src/api/v1/ehr/service.py
@@ -15,7 +15,8 @@ from src.api.v1.ehr.repository import (
     find_composition_by_uid,
     add_deletion_contribution_and_update_ehr,
     find_deletion_contribution_for_version,
-    find_contribution_by_version_uid
+    find_contribution_by_version_uid,
+    find_contribution_by_id
 )
 from src.api.v1.ehr.models import (
     EHRStatus, PartySelf, EHRCreationResponse, EHR, Composition, CompositionCreate,
@@ -23,6 +24,38 @@ from src.api.v1.ehr.models import (
     ObjectRef, HierObjectID
 )
 from src.app.core.models import Contribution, AuditDetails
+
+
+async def retrieve_contribution(ehr_id: str, contribution_uid: str, db: AsyncIOMotorDatabase) -> Contribution:
+    """
+    Retrieves the a specific Contribution for a given EHR.
+
+    It validates that the contribution exists and that it belongs to the specified EHR
+
+    Args:
+        ehr_id: The identifier of the parent EHR.
+        contribution_uid: The unique identifier of the contribution
+        db: The database session.
+
+    Returns:
+        The validated Contribution Pydantic model
+
+    Raises:
+        HTTPException: 404 if the contribution is not found or does not belong to the EHR
+    """
+
+    contribution_doc = await find_contribution_by_id(contribution_uid, db)
+
+    # If the contribution doesn't exist OR it doesn't belong to the specified EHR,
+    # return a 404 to prevent leaking information about the existence of contributions under different EHRs.
+
+    if not contribution_doc or contribution_doc.get("ehr_id") != ehr_id:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Contribution with id '{contribution_uid}' not found in EHR '{ehr_id}'"
+        )
+    
+    return Contribution.model_validate(contribution_doc)
 
 
 async def retrieve_ehr_status_by_ehr_id(ehr_id: str, db: AsyncIOMotorDatabase) -> Tuple[EHRStatus, datetime]:
