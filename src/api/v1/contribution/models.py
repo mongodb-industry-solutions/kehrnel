593bcb38c6da5002dc5a0022e5f5fb67d9951a6b
Giovanni Rodriguez <gio.rodriguez@mongodb.com>
2025-09-26 13:50:51 -0600

    Modified import paths in src/api/v1/contribution/models.py

--- src/api/v1/contribution/models.py
+++ src/api/v1/contribution/models.py
@@ -1,4 +1,4 @@
 # Import the Error response from the erh models
 # TODO: Assess if it's ok this approach if not, let's import it directly from ehr/models.py
 
-from src.api.v1.ehr.models import ErrorResponse
+from src.api.v1.common.models import ErrorResponse
