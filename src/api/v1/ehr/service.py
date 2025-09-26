8a4d499a9ae34a6334f9ef3a90018f0c05b01b91
Giovanni Rodriguez <gio.rodriguez@mongodb.com>
2025-09-26 12:38:06 -0600

    Moved the contribution service functions from ehr/service to contribution/service

--- src/api/v1/ehr/service.py
+++ src/api/v1/ehr/service.py
@@ -31,38 +31,6 @@ from src.api.v1.ehr.models import (
 from src.app.core.models import Contribution, AuditDetails
 
 
-async def retrieve_contribution(ehr_id: str, contribution_uid: str, db: AsyncIOMotorDatabase) -> Contribution:
-    """
-    Retrieves the a specific Contribution for a given EHR.
-
-    It validates that the contribution exists and that it belongs to the specified EHR
-
-    Args:
-        ehr_id: The identifier of the parent EHR.
-        contribution_uid: The unique identifier of the contribution
-        db: The database session.
-
-    Returns:
-        The validated Contribution Pydantic model
-
-    Raises:
-        HTTPException: 404 if the contribution is not found or does not belong to the EHR
-    """
-
-    contribution_doc = await find_contribution_by_id(contribution_uid, db)
-
-    # If the contribution doesn't exist OR it doesn't belong to the specified EHR,
-    # return a 404 to prevent leaking information about the existence of contributions under different EHRs.
-
-    if not contribution_doc or contribution_doc.get("ehr_id") != ehr_id:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Contribution with id '{contribution_uid}' not found in EHR '{ehr_id}'"
-        )
-    
-    return Contribution.model_validate(contribution_doc)
-
-
 async def retrieve_ehr_status_revision_history(
     ehr_id: str,
     db: AsyncIOMotorDatabase
